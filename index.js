7d3b0ef88baaf9a54af2f1f0c687c9069ed4fddb
ANIKETHPAWAR <Anikethpawar05@gmail.com>
2025-03-29 16:08:39 +0530

    problem 2 solved

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const Prompt = require('prompt-sync')()
-const age = Prompt('Enter Your age');
+// const age = Prompt('Enter Your age');
 
-if (age <18){
-    return console.log("You get a 20% discount")
-}
-else if (age >18 && age<=65){
-    return console.log("Normal ticket price applies")
+// if (age <18){
+//     return console.log("You get a 20% discount")
+// }
+// else if (age >18 && age<=65){
+//     return console.log("Normal ticket price applies")
 
-}
-else{
-    console.log("You get a 30% senior discount")
-}
+// }
+// else{
+//     console.log("You get a 30% senior discount")
+// }
+
+// problem 2
+const length = Prompt('Enter length of rectangle :')
+const width = Prompt("Enter width : ")
+
+var area = length * width;
+console.log (`The area of rectangle is ${area}`)
